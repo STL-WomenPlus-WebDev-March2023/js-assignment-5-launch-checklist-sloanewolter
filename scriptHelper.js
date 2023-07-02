1e1e39f8510b5775ea76bce3764e854c2a694046
Sloane Wolter <sloanewolter@gmail.com>
2023-07-01 22:13:22 -0500

    added validation to scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,40 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === "") {
+        return "Empty"
+    } else if (isNaN(testInput) === true) {
+        return "Not a Number"
+    } else if (isNaN(testInput) === false) {
+        return "Is a Number"
+    }  
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+   let pilotInput = document.getElementById("pilotName");
+   let copilotInput = document.querySelector("input[name=copilotName]");
+   let fuelLevelInput =document.querySelector("input[name=fuelLevel]");
+   let cargoLevelInput= document.querySelector("inupt[name=cargoMass]");
+   if (validateInput(pilotInput.value) === "Empty" || validateInput(copilotInput.value) === "Empty" || validateInput(fuelLevelInput.value) === "Empty" 
+   || validateInput(cargoLevelInput.value) === "Empty" ) {
+        alert("All fields are required");
+   };
+   if (validateInput(pilotInput.value) === "Is a Number") {
+        alert ("Invalid Input for Pilot Name");
+   };
+   if (validateInput(copilotInput.value) === "Is a Number") {
+        alert ("Invalid input for Copilot Name");
+   };
+   if (validateInput(fuelLevelInput.value) === "Not a Number") {
+        alert ("Invalid Input for Fuel Level");
+   }
+   if(validateInput(cargoLevelInput.value)==="Not a Number") {
+       alert ("Invalid input for Cargo Mass");
+   }
+})
+   }
 }
 
 async function myFetch() {
